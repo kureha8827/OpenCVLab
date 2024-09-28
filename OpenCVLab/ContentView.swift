2edc411a12a287bf13a39772898d13b27532d3cf
kureha8827 <kurehasub719@gmail.com>
2024-09-28 14:41:26 +0900

    Initial Commit

---
+++ OpenCVLab/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OpenCVLab
+//
+//  Created by kureha8827 on 2024/09/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OpenCVLab/OpenCVLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenCVLabApp.swift
+//  OpenCVLab
+//
+//  Created by kureha8827 on 2024/09/28.
+//
+
+import SwiftUI
+
+@main
+struct OpenCVLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
